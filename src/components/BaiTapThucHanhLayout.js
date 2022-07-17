aff2055d1f511adcfa7b0b638d5f58e29cb3f729
kurei01 <ku12322132@gmail.com>
2022-07-17 10:40:57 +0700

    ex1

---
+++ src/components/BaiTapThucHanhLayout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './header'
+import Banner from './banner'
+import Item from './item'
+import Footer from './footer'
+
+export default function BaiTapThucHanhLayout() {
+  return (
+    <div>
+        <Header/>
+        <Banner/>
+        <Item/>
+        <Footer/>
+    </div>
+  )
+}
